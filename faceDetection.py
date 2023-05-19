9f4ff8dffc8de7231a06fcba410037bb3322d598
Pankaj Phour <pankaj.phour70@gmail.com>
2023-05-19 17:20:38 +0530

    Details added for every line of code

--- faceDetection.py
+++ faceDetection.py
@@ -34,19 +34,25 @@ while True:
     results = faceDetection.process(imgRGB)
     # print(results)
     if results.detections:
+        # Proceeding with the detected face to draw boxes around them 
         for id,detection in enumerate(results.detections):
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
+            # Setting the box coordinates around the detected faces 
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                 int(bboxC.width * iw), int(bboxC.height * ih)
-
+            # Putting the box around the detected face in the frame  
             cv2.rectangle(img, bbox, (255,0,255), 2)
+            # Putting the value of face possibility above the face 
             cv2.putText(img,f'Person: {int(detection.score[0]*100)}%',(bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
 
-
+    # getting the current time 
     cTime = time.time()
+    # getitng the frames value in per second 
     fps = 1/(cTime-pTime)
     pTime = cTime
+    # Showing the frames value in per second on the screen 
     cv2.putText(img,f'FPS: {int(fps)}',(20,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 2)
     cv2.imshow('image',img)
+    # Waiting for a speicific time to check again in the frame 
     cv2.waitKey(10)
